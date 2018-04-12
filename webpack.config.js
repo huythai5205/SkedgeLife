139484ab54e2ffb880b9edfa6572b66464c615d4
huy <kennyt995@gmail.com>
2018-04-11 18:01:59 -0600

    install npm eject

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const debug = process.env.NODE_ENV !== "production";
-const webpack = require('webpack');
-
-module.exports = {
-  context: __dirname,
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: './'
-}
