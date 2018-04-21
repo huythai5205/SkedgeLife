2267665af3bf60ce0e58296fbc37effddfdd38b3
Stephen Marshall <stevemarshall89@yahoo.com>
2018-04-21 17:31:50 -0600

    slider is finished on home page

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -11,7 +11,9 @@ import {
   Col,
   CardPanel,
   Card,
-  Parallax
+  Parallax,
+  Slider,
+  Slide
 } from "react-materialize";
 
 export default class Home extends Component {
@@ -19,35 +21,35 @@ export default class Home extends Component {
     return (
       <div className="home">
         <Row>
-          <Carousel
-            fixedItem={[
-              <button className="btn">MORE</button>,
-              <button className="btn">TRY ME</button>,
-              <button className="btn">CHECK IT OUT</button>
-            ]}
-            options={{
-              fullWidth: true,
-              indicators: true
-            }}
-            images={[
-              "https://www.sas.com/en_us/learn/academic-programs/students/_jcr_content/par/styledcontainer_24bc/image.img.jpg/1517496220627.jpg",
-              "https://cdlnws.a.ssl.fastly.net/image/upload/f_auto,q_auto/v1494865224/Blog/college_students.jpg",
-              "https://ak3.picdn.net/shutterstock/videos/3684833/thumb/1.jpg"
-            ]}
-            // children={[<p>Hey you</p>, <p>Hey you two</p>]}
-          >
-            <div className="parallax-container">
-              <h3>First carousel panel</h3>
-              <p>you could do all kinds of stuff</p>
-              <button className="btn">MORE</button>
-            </div>
-          </Carousel>
+          <Slider>
+            <Slide
+              src="https://www.uwishunu.com/wp-content/uploads/2016/07/Outdoor-Yoga-The-Oval-Image-1280uw.jpg"
+              title="Organize your own event"
+              style={{ paddingTop: 80 }}
+            >
+              <Button>Learn more</Button>
+            </Slide>
+            <Slide
+              src="https://cdlnws.a.ssl.fastly.net/image/upload/f_auto,q_auto/v1494865224/Blog/college_students.jpg"
+              title="Setup a Workshop"
+              style={{ paddingTop: 80 }}
+            >
+              <Button>Learn more</Button>
+            </Slide>
+            <Slide
+              src="http://freelancingamerica.com/wp-content/uploads/2015/12/taliking-open-air.jpg"
+              title="Schedule and get paid for services"
+              style={{ paddingTop: 80 }}
+            >
+              <Button>Learn more</Button>
+            </Slide>
+          </Slider>
         </Row>
         <Row>
           <Col m={4} s={12}>
             <Card
               className="teal lighten-4 black-text"
-              title="Customize your web presence"
+              title="Customize your Web Presence"
               actions={[
                 <a href="#">
                   <Icon className="link-icon">devices</Icon>
@@ -99,14 +101,15 @@ export default class Home extends Component {
           </Col>
         </Row>
         <Row>
-          <div className="parallax-container">
-            <Parallax imageSrc="https://scontent.fapa1-2.fna.fbcdn.net/v/t1.0-9/30704042_10156138948371113_5893660926522949632_n.jpg?_nc_cat=0&oh=4d7f81708cba681211b72868653c84c2&oe=5B6EDBD2" />
-            <p className="grey-text text-darken-3 lighten-3">
-              Parallax is an effect where the background content or image in
-              this case, is moved at a different speed than the foreground
-              content while scrolling.
-            </p>
-          </div>
+          <Parallax
+            imageSrc="https://scontent.fapa1-2.fna.fbcdn.net/v/t1.0-9/30704042_10156138948371113_5893660926522949632_n.jpg?_nc_cat=0&oh=4d7f81708cba681211b72868653c84c2&oe=5B6EDBD2"
+            className="parallax-container"
+          />
+          <p className="parallax-container">
+            Parallax is an effect where the background content or image in this
+            case, is moved at a different speed than the foreground content
+            while scrolling.
+          </p>
         </Row>
       </div>
     );
